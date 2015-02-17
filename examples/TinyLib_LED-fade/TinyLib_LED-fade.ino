30d58d505ce08f7083d6a101707528f324917289
dusjagr <marc@dusseiller.ch>
2015-02-17 18:08:45 +0530

    changed filenames

--- examples/TinyLib_LED-fade/TinyLib_LED-fade.ino
+++ examples/TinyLib_LED-fade/TinyLib_LED-fade.ino
@@ -1,6 +1,8 @@
-#include <QTouchSense.h>
+#include <QTouchADCTiny.h>
 
-int RefPin  = 5;
+int RefPin  = A1;
+int sensePin =  A3;
+int ledPin = PB4;
 
 int ref1 = 0;
 
@@ -8,26 +10,22 @@ const uint16_t ledFadeTable[32] = {0, 0, 0, 1, 1, 2, 3, 3, 4, 5, 6, 7, 9, 10, 12
 
 void setup() {
 
-  Serial.begin(115200);
-  QTouchSense.settings();
-  ref1 = QTouchSense.sense(0, RefPin, 64);
+  //Serial.begin(115200);
+  QTouchSense.init();
+  ref1 = QTouchSense.sense(sensePin, RefPin, 64);
   
 }
 
 void loop() {
 
-  int value1 = (ref1 - QTouchSense.sense(0, RefPin, 16));
+  int value1 = (ref1 - QTouchSense.sense(sensePin, RefPin, 16));
   
   int16_t idx= (value1); // offset probe_val by value of untouched probe
   if(idx<0) idx= 0; // limit the index!!!
   idx/= 1; // scale the index
   if(idx>31) idx= 31; // limit the index!!!
   
-  analogWrite(9, ledFadeTable[idx]);
-  
-  Serial.print(value1);
-  Serial.print(' ');
-  Serial.println(idx);
+  analogWrite(ledPin, ledFadeTable[idx]);
   
   delay(10);
 }
